54d998df1a1e3a420765987932ba9d3a2bfdc4b7
Bisera Ivanovska <Bisera.Ivanovska@netcetera.com>
2019-04-08 11:41:23 +0200

    [VZD017-1893] Java doc added. implementing the original method.

--- src/main/java/com/netcetera/magnolia/auto/config/updates/commands/ScanAndUpdateConfigurationProperties.java
+++ src/main/java/com/netcetera/magnolia/auto/config/updates/commands/ScanAndUpdateConfigurationProperties.java
@@ -4,14 +4,13 @@ import info.magnolia.commands.MgnlCommand;
 import info.magnolia.context.Context;
 
 /**
- *
+ *  Command that scans and updates the configuration properties configured in the content app.
  */
 public class ScanAndUpdateConfigurationProperties extends MgnlCommand {
 
 
-
 	@Override
 	public boolean execute(Context context) throws Exception {
-		return true;
+		return false;
 	}
 }
