e867589965a8088234cf17a0e08ce9f06f13d63a
Emilija Stefanovska <Emilija.Stefanovska@netcetera.com>
2019-04-09 16:51:13 +0200

    [VZD017-1873] added AdvancedConfigUpdatesConstants class

---
+++ src/main/java/com/netcetera/magnolia/auto/config/updates/AdvancedConfigUpdatesConstants.java
@@ -0,0 +1,53 @@
+package com.netcetera.magnolia.auto.config.updates;
+
+public class AdvancedConfigUpdatesConstants {
+
+  /**
+   * Workspace name.
+   */
+  public static final String WORKSPACE = "advancedConfigUpdates";
+
+  public static class Definition {
+    /**
+     * Node type name.
+     */
+    public static final String NODE_TYPE = "definition";
+
+    public static class Property {
+      /**
+       * Property name.
+       */
+      public static final String NAME = "name";
+      /**
+       * Property name.
+       */
+      public static final String PATH = "path";
+      /**
+       * Property name.
+       */
+      public static final String PROPERTY_NAME = "propertyName";
+      /**
+       * Property name.
+       */
+      public static final String PROPERTY_VALUE = "propertyValue";
+    }
+  }
+
+  public static class Email {
+    /**
+     * Node type name.
+     */
+    public static final String NODE_TYPE = "email";
+
+    public static class Property {
+      /**
+       * Property name.
+       */
+      public static final String NAME = "name";
+      /**
+       * Property name.
+       */
+      public static final String EMAIL = "email";
+    }
+  }
+}
